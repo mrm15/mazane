fc47ad481f4ec2d7c79b827d93aca7bf85a10bc9
mrm15 <mrm.learn@gmail.com>
2024-12-19 03:49:24 +0330

    update

--- src/Components/OrdersClient/RequestTab/RequestTabClient.tsx
+++ src/Components/OrdersClient/RequestTab/RequestTabClient.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import useAuth from "../../../hooks/useAuth.tsx";
 import ItemTypesList from "../RequestListTab/ItemTypesList.tsx";
 import TransActionTypeShow from "../RequestListTab/TransActionTypeShow.tsx";
@@ -6,37 +6,46 @@ import SubmitBox from "../RequestListTab/SubmitBox.tsx";
 
 
 const RequestTabClient = () => {
-const {auth} = useAuth()
+    const {auth} = useAuth()
     const socketData = auth?.socketData
-  // buy or sell
-  const [order_type, setOrder_type] = useState("buy");
+    // buy or sell
+    const [order_type, setOrder_type] = useState("buy");
 
-  const [selectedItemType, setSelectedItemType] = useState(undefined);
-  useEffect(() => {
-    if (socketData?.length > 0) {
-      if (!selectedItemType) {
-        setSelectedItemType(socketData[0])
-      }
-    }
-  }, [socketData])
-  return (<div>
-
-    <ItemTypesList
-      myData={socketData}
-      selectedItemType={selectedItemType}
-      setSelectedItemType={setSelectedItemType}
-    />
-    <TransActionTypeShow
-      order_type={order_type}
-      setOrder_type={setOrder_type}
-    />
-    <SubmitBox
-      order_type={order_type}
-      selectedItemType={selectedItemType}
-    />
+    const [selectedItemType, setSelectedItemType] = useState(undefined);
 
 
-  </div>);
+    useEffect(() => {
+        if (socketData?.length > 0) {
+
+            if (!selectedItemType) {
+                setSelectedItemType(socketData[0])
+            } else {
+                // اگه قیمت عوض شد قیمت زیر دستش هم عوض بشه
+                const currentId = selectedItemType.id;
+                const newItem = socketData.find((row: any) => row.id === currentId)
+                setSelectedItemType(newItem)
+            }
+        }
+    }, [socketData])
+
+    return (<div>
+
+        <ItemTypesList
+            myData={socketData}
+            selectedItemType={selectedItemType}
+            setSelectedItemType={setSelectedItemType}
+        />
+        <TransActionTypeShow
+            order_type={order_type}
+            setOrder_type={setOrder_type}
+        />
+        <SubmitBox
+            order_type={order_type}
+            selectedItemType={selectedItemType}
+        />
+
+
+    </div>);
 };
 
 export default RequestTabClient;
